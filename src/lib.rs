53eea51fa253237d2cc1ec02ba5c1a2a8c312b63
Henrik Berg <khp.berg@gmail.com>
2024-07-17 05:44:08 +0200

    renamed easy/medium

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,8 @@ pub mod array_string;
 #[allow(dead_code)]
 pub mod dynamic_programming;
 #[allow(dead_code)]
-pub mod easy_problems;
+pub mod easy;
 #[allow(dead_code)]
 pub mod hard;
 #[allow(dead_code)]
-pub mod medium_problems;
+pub mod medium;
